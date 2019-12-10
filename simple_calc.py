933c35e03aaa372dc74c4edb8dde31cd5d8ab011
Dilan Morar <DMorar@spartaglobal.com>
2019-12-10 10:15:32 +0000

    passing the tests that have been added

--- simple_calc.py
+++ simple_calc.py
@@ -1,9 +1,9 @@
 class simple_calc():
     def add(self, arg1, arg2):
-        return 6
+        return arg1 + arg2
     def subtract(self, arg1, arg2):
-        return 2
+        return arg1 - arg2
     def multiply(self, arg1, arg2):
-        return 6
+        return arg1 * arg2
     def divide(self, arg1, arg2):
-        return 2
+        return arg1 / arg2
